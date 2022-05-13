b6f1a0ae030e72cc18f5b255bd54e1d6f5749790
Aarya <amathrej@asu.edu>
2022-05-13 03:40:32 -0700

    Rename output array to choices in computerPlay function

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 function computerPlay() {
     let randomNumber = getRandomInt(3);
-    let output = ["Rock","Paper","Scissors"];
-    return output[randomNumber];
+    let choices = ["Rock","Paper","Scissors"];
+    return choices[randomNumber];
 }
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+
+  function playRound(playerSelection, computerSelection){
+    let correctPlayerSelection = playerSelection.toLowerCase();
+    let gameOptions = ["Rock","Paper","Scissors"];
+  }
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
