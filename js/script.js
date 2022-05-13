d2029693d7a6ecb0c68b71430d49732e006a4566
Aarya <amathrej@asu.edu>
2022-05-13 04:27:35 -0700

    Finish game() function

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ function getRandomInt(max) {
   function playRound(playerSelection, computerSelection){
     let computerAnswer = computerSelection.toLowerCase();
     let playerAnswer = playerSelection.toLowerCase();
-    playRoundHelper(playerAnswer, computerAnswer);
+    return playRoundHelper(playerAnswer, computerAnswer);
 
   }
 
@@ -62,6 +62,27 @@ function getRandomInt(max) {
     }
   }
 
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-playRound(playerSelection, computerSelection);
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    let gameOutput = 0;
+    let continueGame = 1;
+    while (continueGame === 1){
+        let playerSelection = prompt("Pick Your Poison. (Rock, Paper or Scissors)");
+        let computerSelection = computerPlay();
+        gameOutput = playRound(playerSelection, computerSelection);
+
+        if(gameOutput == 1) playerScore += 1;
+        else if (gameOutput == -1) computerScore += 1;
+
+        if(playerScore == 5)
+        {
+            continueGame = 0;
+            console.log("You have won the game!");
+        }
+        else if (computerScore == 5){
+            continueGame = 0;
+            console.log("You have lost the game!");
+        }
+     }
+}
