a5a4f10b7412f093b2ec0e18d5852a0a0f46c44d
Aarya <amathrej@asu.edu>
2022-05-13 03:57:28 -0700

    Finish playRound() function

--- js/script.js
+++ js/script.js
@@ -9,10 +9,28 @@ function getRandomInt(max) {
   }
 
   function playRound(playerSelection, computerSelection){
-    let correctPlayerSelection = playerSelection.toLowerCase();
-    let gameOptions = ["Rock","Paper","Scissors"];
+    let computerAnswer = computerSelection.toLowerCase();
+    let playerAnswer = playerSelection.toLowerCase();
+    
+    if(playerAnswer === "rock"){
+        if(computerAnswer === "paper") return "You Lose! Paper beats Rock";
+        else if(computerAnswer === "scissors") return "You Win! Rock beats Scissors";
+        else return "It's a Draw"; 
+    }
+    else if(playerAnswer === "paper"){
+        if(computerAnswer === "scissors") return "You Lose! Scissors beats Paper";
+        else if(computerAnswer === "rock") return "You Win! Paper beats Rock";
+        else return "It's a Draw"; 
+    }
+    else if(playerAnswer === "scissors"){
+        if(computerAnswer === "rock") return "You Lose! Rock beats Scissors";
+        else if(computerAnswer === "paper") return "You win! Scissors beats Paper";
+        else return "It's a Draw"; 
+    }
+
   }
 
 const playerSelection = "rock";
 const computerSelection = computerPlay();
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
