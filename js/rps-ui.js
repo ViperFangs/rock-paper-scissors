9a82520ad7e640d60432bc4300c0a4498d4a72e4
Aarya <amathrej@asu.edu>
2022-05-15 20:23:11 -0700

    Change textContent of score for click event

--- js/rps-ui.js
+++ js/rps-ui.js
@@ -1,4 +1,5 @@
 let button = document.querySelectorAll('button');
+let score = document.getElementById('score');
 
 function computerPlay() {
     let randomNumber = getRandomInt(3);
@@ -19,45 +20,45 @@ function playRound(playerSelection, computerSelection){
 function playRoundHelper(playerAnswer, computerAnswer){
     if(playerAnswer === "rock"){
         if(computerAnswer === "paper") {
-            console.log("You Lose! Paper beats Rock");
+            score.textContent = "You Lose! Paper beats Rock";
             return -1;
         }
         else if(computerAnswer === "scissors"){
-            console.log("You Win! Rock beats Scissors");
+            score.textContent = "You Win! Rock beats Scissors";
             return 1;
         }
         else {
-            console.log("It's a Draw");
+            score.textContent = "It's a Draw";
             return 0; 
         }
     }
 
     else if(playerAnswer === "paper"){
         if(computerAnswer === "scissors") {
-            console.log("You Lose! Scissors beats Paper");
+            score.textContent = "You Lose! Scissors beats Paper";
             return -1;
         }
         else if(computerAnswer === "rock"){
-            console.log("You Win! Paper beats Rock");
+            score.textContent = "You Win! Paper beats Rock";
             return 1;
         }
         else {
-            console.log("It's a Draw");
+            score.textContent = "It's a Draw";
             return 0; 
         } 
     }
 
     else if(playerAnswer === "scissors"){
         if(computerAnswer === "rock") {
-            console.log("You Lose! Rock beats Scissors");
+            score.textContent = "You Lose! Rock beats Scissors";
             return -1;
         }
         else if(computerAnswer === "paper"){
-            console.log("You Win! Scissors beats Paper");
+            score.textContent = "You Win! Scissors beats Paper";
             return 1;
         }
         else {
-            console.log("It's a Draw");
+            score.textContent = "It's a Draw";
             return 0; 
         }
     }
