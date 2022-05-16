3f3e53d391b1c5400c873f9bd4ca262fed6f26b6
Aarya <amathrej@asu.edu>
2022-05-15 19:55:01 -0700

    Copy playRoundHelper code from script.js

--- js/rps-ui.js
+++ js/rps-ui.js
@@ -14,12 +14,60 @@ function playRound(playerSelection, computerSelection){
     return playRoundHelper(playerAnswer, computerAnswer);
     }
 
+function playRoundHelper(playerAnswer, computerAnswer){
+    if(playerAnswer === "rock"){
+        if(computerAnswer === "paper") {
+            console.log("You Lose! Paper beats Rock");
+            return -1;
+        }
+        else if(computerAnswer === "scissors"){
+            console.log("You Win! Rock beats Scissors");
+            return 1;
+        }
+        else {
+            console.log("It's a Draw");
+            return 0; 
+        }
+    }
+
+    else if(playerAnswer === "paper"){
+        if(computerAnswer === "scissors") {
+            console.log("You Lose! Scissors beats Paper");
+            return -1;
+        }
+        else if(computerAnswer === "rock"){
+            console.log("You Win! Paper beats Rock");
+            return 1;
+        }
+        else {
+            console.log("It's a Draw");
+            return 0; 
+        } 
+    }
+
+    else if(playerAnswer === "scissors"){
+        if(computerAnswer === "rock") {
+            console.log("You Lose! Rock beats Scissors");
+            return -1;
+        }
+        else if(computerAnswer === "paper"){
+            console.log("You Win! Scissors beats Paper");
+            return 1;
+        }
+        else {
+            console.log("It's a Draw");
+            return 0; 
+        }
+    }
+    }
+
 let button = document.querySelectorAll('button');
 
 button.forEach(item => {
     item.addEventListener('click', event => {
         if(item.getAttribute('id') == "rock"){
             console.log('rock');
+            playRound("rock", computerPlay());
         }
         
         if(item.getAttribute('id') == "paper"){
